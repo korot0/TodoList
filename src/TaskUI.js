ad59e9db232ee6c243f19aa1c032cc96fa2c01c5
korot0 <108902180+korot0@users.noreply.github.com>
2025-05-22 13:35:49 -0500

    Refactor

--- src/TaskUI.js
+++ src/TaskUI.js
@@ -16,12 +16,17 @@ const createTaskElement = (taskTitle, taskPriority, list) => {
   input.classList.add("form-check-input");
   input.type = "checkbox";
   input.id = formatTaskID(taskTitle);
+
   const label = document.createElement("label");
   label.classList.add("ms-2");
   label.htmlFor = input.id;
   label.textContent = taskTitle;
 
-  const deleteBtn = createDeleteButton(taskTitle, list);
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("material-symbols-outlined", "hide", "delete-btn");
+  deleteBtn.textContent = "delete";
+  deleteBtn.value = taskTitle;
+  attachDeleteBtnListener(deleteBtn, list);
 
   li.append(input, label, deleteBtn);
   return li;
@@ -29,19 +34,9 @@ const createTaskElement = (taskTitle, taskPriority, list) => {
 
 const formatTaskID = (taskTitle) => taskTitle.replace(/\s+/g, "-");
 
-const createDeleteButton = (taskTitle, list) => {
-  const button = document.createElement("button");
-  button.classList.add("material-symbols-outlined", "hide", "delete-btn");
-  button.textContent = "delete";
-  button.value = taskTitle;
-  setupDeleteBtnListener(button, list);
-  return button;
-};
-
-const setupDeleteBtnListener = (button, list) => {
+const attachDeleteBtnListener = (button, list) => {
   button.addEventListener("click", () => {
-    const taskTarget = button.value;
-    list.removeTask(taskTarget);
+    list.removeTask(button.value);
     updateScreen();
   });
 };
@@ -50,9 +45,4 @@ const stylePriority = (priority) => {
   if (priority === "medium") return "list-group-item-warning";
   if (priority === "high") return "list-group-item-danger";
   // "list-group-item-success" for green
-};
-
-/* Reset UI */
-export const resetTasksUI = () => {
-  document.querySelector().textContent = "";
 };
